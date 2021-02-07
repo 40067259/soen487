c1f89e5e6c4d4e1147427023f0a60cddd0cd2255
EdwardMeng <edwardameng@126.com>
2021-02-07 22:08:44 +0800

    add AlbumClient

---
+++ assignment1/src/main/java/com/example/a1/albumClient/MyResponseHandler.java
@@ -0,0 +1,35 @@
+package com.example.a1.albumClient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MyResponseHandler implements ResponseHandler<String> {
+
+    @Override
+    public String handleResponse(final HttpResponse httpResponse) throws ClientProtocolException, IOException {
+
+        int status = httpResponse.getStatusLine().getStatusCode();
+        if(status >= 200&&status < 300)
+        {
+            HttpEntity entity = httpResponse.getEntity();
+            if(entity == null)
+            {
+                return "";
+            }
+            else
+                return EntityUtils.toString(entity);
+        }
+        else
+            return status + "";
+    }
+
+
+}
